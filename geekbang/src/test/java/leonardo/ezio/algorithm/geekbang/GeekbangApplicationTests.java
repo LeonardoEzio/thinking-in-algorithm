bd121101eb70e5519cc06f053715d63ac09351b1
LeonardoEzio <LeonardoEzio@163.com>
2020-07-15 21:47:37 +0800

    Init commit

---
+++ geekbang/src/test/java/leonardo/ezio/algorithm/geekbang/GeekbangApplicationTests.java
@@ -0,0 +1,13 @@
+package leonardo.ezio.algorithm.geekbang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeekbangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ leetcode/src/main/java/leonardo/ezio/algorithm/leetcode/test/TestMain.java
@@ -0,0 +1,8 @@
+package leonardo.ezio.algorithm.leetcode.test;
+
+public class TestMain {
+
+    public static void main(String[] args) {
+        System.out.println("test main ");
+    }
+}

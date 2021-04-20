8e486bbdac767dec02007dae89849569b3cc91ba
LeonardoEzio <LeonardoEzio@163.com>
2021-04-20 23:10:05 +0800

    更新题解

---
+++ leetcode/src/main/java/leonardo/ezio/datastructure/ListNode.java
@@ -0,0 +1,24 @@
+package leonardo.ezio.datastructure;
+
+/**
+ * @ClassName : ListNode
+ * @Author : LeonardoEzio
+ * @Date : 2021-04-20 22:50
+ * @Description : Leet Code 链表题 通用数据结构
+ */
+public class ListNode {
+
+    public int val;
+
+    public ListNode next;
+
+    public ListNode(int val) {
+        this.val = val;
+    }
+
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+
+}
